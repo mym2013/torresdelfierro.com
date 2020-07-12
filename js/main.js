be067823b9506bf5f6c42c8426612687cd28dea2
mym2013 <torresdelfierro@gmail.com>
2020-07-12 00:27:13 -0400

    agregando otras paginas, y modificando el json del main.js agregando link dinpamico

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@ $(document).ready(function(){
       var posts=[
         { title:' Nuestra Historia tdchile ingeniería',
           date: "publicado el día " + moment().date() +" "+ moment().format("MMMM") + "  del año " + moment().format("YYYY"),
-          content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur metus ex, eget vulputate leo volutpat in. Maecenas bibendum, nunc et porttitor faucibus, ex mi venenatis dolor, pretium dapibus orci neque sit amet dolor. Nam interdum massa id lacus ultrices malesuada. In massa arcu, efficitur rutrum iaculis ac, porttitor pharetra arcu. Proin laoreet eleifend lectus, rutrum ultricies tortor tincidunt id. Praesent vitae lacinia felis. Quisque commodo velit tempus enim laoreet, a posuere magna luctus. Duis et pellentesque lorem. Proin consequat vitae libero nec pellentesque."
+          content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur metus ex, eget vulputate leo volutpat in. Maecenas bibendum, nunc et porttitor faucibus, ex mi venenatis dolor, pretium dapibus orci neque sit amet dolor. Nam interdum massa id lacus ultrices malesuada. In massa arcu, efficitur rutrum iaculis ac, porttitor pharetra arcu. Proin laoreet eleifend lectus, rutrum ultricies tortor tincidunt id. Praesent vitae lacinia felis. Quisque commodo velit tempus enim laoreet, a posuere magna luctus. Duis et pellentesque lorem. Proin consequat vitae libero nec pellentesque.",
+          leermas:'C:/Users/Gonzalo/Desktop/myWeb/torresdelfierro_cl/paginas/sobre_tdchile.html',
         },
         { title:'Nuestros Clientes',
           date: "publicado el día " + moment().date() +" "+ moment().format("MMMM") + "  del año " + moment().format("YYYY"),
@@ -67,7 +68,7 @@ posts.forEach((item, index) => {
       <p>
         ${item.content}
       </p>
-      <a href="#" class="button-more">Leer más</a>
+      <a href="${item.leermas}" class="button-more">Leer más</a>
     </article>
   `;
 
